6fbb814a4e4f091065749e5008451800ee092144
tgolan14 <tgolan14@gmail.com>
2020-01-21 17:52:01 +0200

    ya man yak

--- screen_boggle.py
+++ screen_boggle.py
@@ -6,10 +6,12 @@ class Screen_Boggle:
 
     def __init__(self,filename):
 
-        self._root = tk.Tk()
-        self._root.title("BEST BOGGLE GAME EVER")
+        self.__root = tk.Tk()
+        self.__root.title("BEST BOGGLE GAME EVER")
         self.__game_runner = GameRunner(filename)
         self.init_buttons(self.__game_runner.get_board())
+        self.__near_buttons = {}
+        self.__pressed_buttons = []
 
 
         # here we need to add the label that will count the time
@@ -43,35 +45,42 @@ class Screen_Boggle:
         # לבדוק אם המקש רציף אם הלחיצה הקודמת
         # אם כן להמשיך את הרצף ולשנות את ערך המחרזות
         # אם לא לשנות את המסך בהתאם ולהתחיל מחרוזת חדשה
+        # button hasn't pressed already,and it's a new guess or legal continue
+        if button not in self.__pressed_buttons and \
+            (not self.__pressed_buttons or \
+            button in self.__near_buttons[self.__pressed_buttons[-1]]):
+            button.configure(bg = "red")
+            self.__pressed_buttons.append(button) # add to the pressed list
+            self.__game_runner.__cur_guess += button.text
+        else:
+            self.unpress_all()
+
     def guess_pressed(self):
         # הפונקציה צריכה לבדוק אם המחרוזת המנוחשת נמצאת במילון
         # אם כן לשנות את הניקוד בהתאם אם לא להדפיס הודעה שגיאה רלוונטית
+        if self.__game_runner.check_guess():
+            self.__game_runner.update_score(len(self.guess_pressed()))
+            self.update_score_board()
+        self.unpress_all()
+
+
     def quit_pressed(self):
         # הפונקציה סוגרת את המשחק (או מדפיסת הודעה לברר ששואלת אם להתחיל מחדש או לפרוש)
+
     def end_of_time(self):
         # הפונקציה מטפלת במקרה והזמן נגמר
         # הפונקציה תקושר לפונקציה after שמקבלת זמן ריצה ולאחריו מפעילה את הפונקציה הקשורה
         # תדפיס הודעת שגיאה מתאימה וכו׳
 
+    def unpress_all(self):
+        """
+        This method unpress all the letters buttons,
+         and nullify the current guess
+        """
+        for button in self.__pressed_buttons:
+            button.configure(bg='')
+        self.__pressed_buttons = []
+        self.__game_runner.__cur_guess = ""
+
 if __name__ == '__main__':
-
-    root = tk.Tk()
-    root.title("BEST BOGGLE GAME EVER")
-    randomize_board()
-    for row_index, row in enumerate(randomize_board()):
-        for letter_index, letter in enumerate(row):
-            if letter != "QU":
-                new_button = tk.Button(root, text=letter, font=("Courier", 30),
-                                       height=2, width=2)
-            else:
-                new_button = tk.Button(root, text=letter, font=("Courier", 30), height=2, width=2)
-
-            new_button.grid(row=row_index, column=letter_index)
-
-
-
-    root.mainloop()
-
-
-
-
+    pass
