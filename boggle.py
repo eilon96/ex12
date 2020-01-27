316ce7988a27efb34fe4008a2f761b9b65fe5fa4
tgolan14 <tgolan14@gmail.com>
2020-01-27 20:25:19 +0200

    ya man yak

--- boggle.py
+++ boggle.py
@@ -6,10 +6,10 @@ import math
 
 class GameRunner:
 
-    def __init__(self,filename):
+    def __init__(self, filename):
 
-        file = open(filename,"r")
-        self.__dictionary  = file.read().split("\n")
+        file = open(filename, "r")
+        self.__dictionary = file.read().split("\n")
         self.__board = randomize_board()
         self.__score = 0
         self.__time = 0
@@ -17,8 +17,6 @@ class GameRunner:
         self.__cur_guess = ""
         self.__last_button_pressed = None
         file.close()
-
-
 
     """
 
@@ -28,40 +26,48 @@ class GameRunner:
 
     def get_dictionary(self):
         return self.__dictionary
+
     def get_cur_guess(self):
         return self.__cur_guess
+
     def get_score(self):
         return self.__score
+
     def get_board(self):
         return self.__board
+
     def get_points(self):
         return self.__points
+
     def get_time(self):
         return self.__time
+
     def get_founded_words_list(self):
         return self.__founded_words_list
+
     def get_last_button_pressed(self):
         return self.__last_button_pressed
-    def set_cur_guess(self,letter):
+
+    def set_cur_guess(self, letter):
         if letter != "":
             self.__cur_guess += letter
         else:
             self.__cur_guess = ""
-
 
     """
     all relevant set functions, the dictionary and board should not be changed 
     so they do not have a set function 
     """
 
-    def set_points(self,points):
+    def set_points(self, points):
         """
-        :param points: get an amount of points to add to the current amounts of points
+        :param points: get an amount of points to add to the current amounts of
+        points
         :return: the function adds the points to the points variable
         """
         self.__points += points
 
-    def set_time(self,time):
+    def set_time(self, time):
         """
         :param time: gets an amount of time and adds it to the time variable
         """
@@ -75,31 +81,32 @@ class GameRunner:
 
         self.__founded_words_list.append(self.__cur_guess)
 
-    def set_last_button_pressed(self,new_pressed_button):
+    def set_last_button_pressed(self, new_pressed_button):
         """
 
-        :param new_pressed_button: the function gets a tuple representing the coordinates
-                of the button that was pressed by the player and set it as the last pressed button
+        :param new_pressed_button: the function gets a tuple representing the
+        coordinates of the button that was pressed
+        by the player and set it as the last pressed button
         :return:
         """
         self.__last_button_pressed = new_pressed_button
 
-
-
-    def is_press_ok(self,new_letter_crd):
+    def is_press_ok(self, new_letter_crd):
         """
-        :param lst_let_crd: a tuple representing the coordinate of the last letter that was picked
-        :param new_let_crd: a tuple representing the coordinate of the new letter that was picked
-        :return: the function returns True if the player picked a legal letter and False if not
+        :param lst_let_crd: a tuple representing the coordinate of
+        the last letter that was picked
+        :param new_let_crd: a tuple representing
+        the coordinate of the new letter that was picked
+        :return: the function returns True if the player
+        picked a legal letter and False if not
         """
         if self.__last_button_pressed == None:
             return True
-        elif abs(self.get_last_button_pressed()[0]-new_letter_crd[0]) >= 2:
+        elif abs(self.get_last_button_pressed()[0] - new_letter_crd[0]) >= 2:
             return False
-        elif abs(self.get_last_button_pressed()[1]-new_letter_crd[1]) >= 2:
+        elif abs(self.get_last_button_pressed()[1] - new_letter_crd[1]) >= 2:
             return False
         return True
-
 
     def is_word_in_dict(self):
         """
@@ -110,7 +117,7 @@ class GameRunner:
         """
         return self.__cur_guess in self.get_dictionary()
 
-    def is_repeating_word(self,word):
+    def is_repeating_word(self, word):
         """
 
         :param words_found_list: a list of words that  have already been found
@@ -118,23 +125,24 @@ class GameRunner:
         :return: the function returns True if the word was already found and False otherwise
         """
         return word in self.get_founded_words_list()
+
     def try_word(self):
         """
 
         :return: the function checks if the current guess is a good one and returns true if it is a good one and false
         otherwise
         """
-        if self.is_word_in_dict() and not self.is_repeating_word(self.__cur_guess):
+        if self.is_word_in_dict() and not self.is_repeating_word(
+                self.__cur_guess):
             self.add_founded_words_list()
             return True
         else:
             return False
 
     def update_score(self):
-        self.__score += len(self.__cur_guess)**2
+        self.__score += len(self.__cur_guess) ** 2
 
     def did_time_passed(self):
-        if self.__time > 60*3:
+        if self.__time > 60 * 3:
             return True
         return False
-
--- screen_boggle.py
+++ screen_boggle.py
@@ -31,25 +31,32 @@ class Screen_Boggle:
             for letter_index, letter in enumerate(row):
 
                 if letter != "QU":
-                    new_button = tk.Button(self._root, text=letter, font=("Courier", 30),
-                                           height=2, width=2)
-                    new_button.config(command=self.letter_pressed_event(new_button,letter,row_index,letter_index))
+                    new_button = tk.Button(self._root, text=letter,
+                                      font=("Courier", 30),height=2, width=2)
+                    new_button.config(command=self.letter_pressed_event
+                    (new_button,letter,row_index,letter_index))
                 else:
-                    new_button = tk.Button(self._root, text=letter, font=("Courier", 30), height=2, width=2)
-                    new_button.config(command=self.letter_pressed_event(new_button,letter,row_index,letter_index))
+                    new_button = tk.Button(self._root, text=letter,
+                                    font=("Courier", 30), height=2, width=2)
+                    new_button.config(command=self.letter_pressed_event
+                    (new_button,letter,row_index,letter_index))
 
                 new_button.grid(row=row_index, column=letter_index)
 
         # creates the quit button and guess button
-        quit_button = tk.Button(self._root, text="Quit", font=("Courier", 32), width=4,command=self.quit_pressed)
+        quit_button = tk.Button(self._root, text="Quit", font=("Courier", 32),
+                                width=4,command=self.quit_pressed)
         quit_button.place(y=263)
-        Try_button = tk.Button(self._root, text="Try", font=("Courier", 32), width=4,command=self.guess_pressed)
+        Try_button = tk.Button(self._root, text="Try", font=("Courier", 32),
+                               width=4,command=self.guess_pressed)
         Try_button.place(x=84, y=263)
 
     def init_labels(self):
-        self.__time_label = tk.Label(self._root, text="10:10", font=("Courier", 22), width=11, bg="black", fg="white")
+        self.__time_label = tk.Label(self._root, text="10:10",
+                        font=("Courier", 22), width=11, bg="black", fg="white")
         self.__time_label.place(x=1, y=303)
-        self.__points_label = tk.Label(self._root, text="100", font=("Courier", 22), width=11, bg="white", fg="black")
+        self.__points_label = tk.Label(self._root, text="100",
+                        font=("Courier", 22), width=11, bg="white", fg="black")
         self.__points_label.place(x=1, y=343)
 
 
@@ -80,7 +87,8 @@ class Screen_Boggle:
         # אם כן לשנות את הניקוד בהתאם אם לא להדפיס הודעה שגיאה רלוונטית
         if self.__game_runner.try_word():
             self.__game_runner.update_score()
-            self.__points_label.config(text=str(self.__game_runner.get_score()))
+            self.__points_label.config\
+                (text=str(self.__game_runner.get_score()))
         self.unpress_all()
         self.__game_runner.set_cur_guess("")
 
@@ -95,7 +103,9 @@ class Screen_Boggle:
 
     def update_time(self):
         self.__game_runner.set_time(10)
-        self.__time_label.config(text=f"0{self.__game_runner.get_time()//60}:{(self.__game_runner.get_time()%60)//10}{(self.__game_runner.get_time()%60)%10}")
+        self.__time_label.config(text=f"0{self.__game_runner.get_time()//60}:"
+                              f"{(self.__game_runner.get_time()%60)//10}"
+                              f"{(self.__game_runner.get_time()%60)%10}")
         if not self.__game_runner.did_time_passed():
             self._root.after(1000, self.update_time)
         else:
@@ -107,12 +117,15 @@ class Screen_Boggle:
         self.end_of_time.title("Your Time is Up")
         self.end_of_time.geometry("200x100")
         self.end_of_time.resizable(0, 0)
-        end_of_time_label = tk.Label(self.end_of_time,text="to continue press continue to \n"
-                                                      "quit well you know what to do... ")
+        end_of_time_label = tk.Label(self.end_of_time,
+                                text="to continue press continue to \n"
+                                         "quit well you know what to do... ")
         end_of_time_label.pack()
-        quit_button = tk.Button(self.end_of_time, text="Quit", font=("Courier", 20), width=5,command=self.quit_pressed)
+        quit_button = tk.Button(self.end_of_time, text="Quit", font=("Courier",
+                                        20), width=5,command=self.quit_pressed)
         quit_button.place(x=10,y=50)
-        continue_button = tk.Button(self.end_of_time, text="continue", font=("Courier", 20), width=8,
+        continue_button = tk.Button(self.end_of_time, text="continue",
+                                    font=("Courier", 20), width=8,
                                     command=self.continue_button_pressed)
         continue_button.place(x=85,y=50)
         self.end_of_time.mainloop()
@@ -141,7 +154,7 @@ class Screen_Boggle:
 
 if __name__ == '__main__':
     root = tk.Tk()
-    screen = Screen_Boggle("boggle_dict.txt",root)
+    screen = Screen_Boggle("boggle_dict.txt", root)
     root.mainloop()
 
 
